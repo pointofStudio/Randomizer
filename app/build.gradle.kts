5684389725edfa3d4ea5a1fbe948f383a087372e
pointofStudio <albertomasoch@gmail.com>
2025-01-30 19:44:37 +0100

    Fixed various bugs. Fixed Home Screen. Moved the Crash button. Fixed dices images. Fixed texts. Fixed screen orientation. A special thank to Gab. Grossi a tester who found and report various bug.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 2
-        versionName = "2"
+        versionName = "2.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
